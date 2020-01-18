add9029168d640c22dd649729d39426a3df1c192
Marcio Silva <marcioadsworks@gmail.com>
2020-01-17 21:16:16 -0300

    second component with arrow function

---
+++ src/components/SecondComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+
+const second = () =>
+    <div>
+        <h1>Second Component with arrow function</h1>
+    </div>
+
+export default second;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import ReactDOM from 'react-dom'
 import React from  'react'
 import FirstComponent from './components/FirstComponent'
-
+import SecondComponent from './components/SecondComponent'
 const element = document.getElementById('root')
 ReactDOM.render(
     <div>
     <FirstComponent/>
+    <SecondComponent/>
     </div>
 ,element)
 
