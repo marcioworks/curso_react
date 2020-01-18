6ac793b0c60de5c84cb658123c8a5780b21107d9
Marcio Silva <marcioadsworks@gmail.com>
2020-01-17 21:27:41 -0300

    third component: using props

---
+++ src/components/ThirdComponent.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const third = (props) =>
+    <div>
+        <h1>Props: {props.valor}</h1>
+    </div>
+
+    export default third;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import ReactDOM from 'react-dom'
 import React from  'react'
 import FirstComponent from './components/FirstComponent'
 import SecondComponent from './components/SecondComponent'
+import ThirdComponent from './components/ThirdComponent'
+
+
+
+
 const element = document.getElementById('root')
 ReactDOM.render(
     <div>
     <FirstComponent/>
     <SecondComponent/>
+    <ThirdComponent valor ="estou usando props" />
     </div>
 ,element)
 
