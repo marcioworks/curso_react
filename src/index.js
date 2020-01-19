ef843ad888a4279b831173f0cab6a2ffbcd9100a
Marcio Silva <marcioadsworks@gmail.com>
2020-01-19 10:30:55 -0300

    Class component

--- src/index.js
+++ src/index.js
@@ -1,27 +1,28 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
-import FirstComponent from './components/FirstComponent'
-import SecondComponent from './components/SecondComponent'
-import ThirdComponent from './components/ThirdComponent'
-import { CompA, CompB } from './components/MultComponents'
-import Family from './components/Family'
-import Son from './components/SonComponent'
-import Father from './components/Father'
-
+// import FirstComponent from './components/FirstComponent'
+// import SecondComponent from './components/SecondComponent'
+// import ThirdComponent from './components/ThirdComponent'
+// import { CompA, CompB } from './components/MultComponents'
+// import Family from './components/Family'
+// import SonComponent from './components/SonComponent'
+// import Father from './components/Father'
+import ClassComponent from './components/ClassComponent'
 
 const element = document.getElementById('root')
 ReactDOM.render(
     <div>
-        <FirstComponent />
+        <ClassComponent valor="Sou um component de classe"/>
+        {/* <FirstComponent />
         <SecondComponent />
         <ThirdComponent valor="estou usando props" />
         <CompA value="hi i'm A" />
         <CompB value="B on the Area" />
         <Family surname="Bergs">
-            <Son name="Jonh"/>
-            <Son name="Joane"/>
+            <SonComponent name="Jonh"/>
+            <SonComponent name="Joane"/>
         </Family>
-        <Father/>
+        <Father/> */}
     </div>
     , element)
 
